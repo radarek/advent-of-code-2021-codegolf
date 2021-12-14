def71ac2699b3ab336343fc8fe0fc192399ceb7c
Radosław Bułat <radek.bulat@gmail.com>
2021-12-14 02:27:31 +0100

    Day4: Giant Squid - part1

---
+++ day4/part1/_.rb
@@ -0,0 +1,13 @@
+# ideas:
+# squeezing spaces /\s+/ => ' '
+
+
+
+# s =~ /\n(^(..\s){1} x (..\s){3}){5}/
+# n=gets;s=gets p;s.scan(/\d+/){}
+# a=gets.split',';gets p;a.each{}
+# n=gets;s=gets p;s.split(?,){}
+# s=gets.split(?,){s||=gets p;}
+# gets.split(?,,(gets p;0)){}
+# gets.scan((gets p;/\d+/)){}
+# gets.split(gets(p)&&?,){}
