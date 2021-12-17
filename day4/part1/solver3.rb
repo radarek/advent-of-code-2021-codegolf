ed08338a08474e4466e0ccb81bb8dc893ced8686
Radosław Bułat <radek.bulat@gmail.com>
2021-12-17 15:22:21 +0100

    Day4: Giant Squid - part1 bugfix

--- day4/part1/solver3.rb
+++ day4/part1/solver3.rb
@@ -1,3 +1,3 @@
 #!ruby -lnaF,
-gets p;$F.map{|n|gsub /(\s) ?#{n}\b/,'\1 x';14.times{(v= ~/^(.x.){5}|(^.{#{_1}}x[^\n]*?.){5}/m)&&eval("p (#{$_[v-v%76,75].tr"x 
+gets p;$F.map{|n|gsub /(\s) ?#{n}\b/,'\1 x';14.times{(v= ~/^(.x.){5}|(^.{#{_1}}x[^\n]*?.){5}/m)&&eval("p (#{$_[v-v%76,76].tr"x 
 ",?+}+0)*#{n};exit")}}
